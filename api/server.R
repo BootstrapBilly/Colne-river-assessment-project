c5559489a96aed6d3486b63d1629d5a4d5f30213
billy <billyjcatchpole@gmail.com>
2024-08-07 08:14:06 +0100

    build: bind to 0.0.0.0

--- api/server.R
+++ api/server.R
@@ -2,7 +2,7 @@ library(plumber)
 
 r <- plumb("api/api.R")
 
-r$run(port = 8000)
+r$run(host = "0.0.0.0", port = 8000)
 
 # You can run this server from the terminal with the following command (ensure that you terminal is at the root of this project)
 
