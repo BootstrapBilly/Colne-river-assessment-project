6a5006b448a1e93c7c2ba4086f32020c72568770
billy <billyjcatchpole@gmail.com>
2024-08-07 07:51:36 +0100

    build: dockerize api

--- api/server.R
+++ api/server.R
@@ -3,3 +3,7 @@ library(plumber)
 r <- plumb("api/api.R")
 
 r$run(port = 8000)
+
+# You can run this server from the terminal with the following command (ensure that you terminal is at the root of this project)
+
+# Rscript api/server.R
