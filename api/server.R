ccf706f0c110cf81cb85cb3812a16de0b9e42ca0
billy <billyjcatchpole@gmail.com>
2024-08-07 18:32:19 +0100

    add cors settings for server

--- api/server.R
+++ api/server.R
@@ -2,6 +2,15 @@ library(plumber)
 
 r <- plumb("api/api.R")
 
+cors <- function(req, res) {
+    res$setHeader("Access-Control-Allow-Origin", "*")
+    res$setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    res$setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    plumber::forward()
+}
+
+r$registerHook("preroute", cors)
+
 r$run(host = "0.0.0.0", port = 8000)
 
 # You can run this server from the terminal with the following command (ensure that you terminal is at the root of this project)
