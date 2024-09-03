1cea81042766cfecdd06184e79fb256b966649b0
billy <billyjcatchpole@gmail.com>
2024-09-03 19:59:25 +0100

    fix api url

--- client/src/pages/map.tsx
+++ client/src/pages/map.tsx
@@ -14,7 +14,7 @@ import { useQuery } from "@tanstack/react-query";
 let apiUrl = "https://crap-api-635719a27ef1.herokuapp.com";
 
 if (import.meta.env.PROD) {
-  apiUrl = "https://crap-api-prod-2e20909e19a5.herokuapp.com/";
+  apiUrl = "https://crap-api-prod-2e20909e19a5.herokuapp.com";
 }
 
 export const Map = () => {
